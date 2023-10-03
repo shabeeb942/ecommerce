8cc682cba852ee3a72a25c6d9bfa37b1513cddfd
shabeeb942 <shabeebsha942@gmail.com>
2023-10-03 11:01:29 +0530

    verbose_name added

--- products/models.py
+++ products/models.py
@@ -2,29 +2,35 @@ from django.db import models
 
 # Create your models here.
 
-class Categorie(models.Model):
+class Categories(models.Model):
     category = models.CharField(max_length=120)
 
+
     class Meta:
-        ordering = ('category',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['category']
 
     def __str__(self):
         return self.category
 
 
-class SubCategorie(models.Model):
-    category = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+class SubCategories(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     sub_category = models.CharField(max_length=120)
 
+
     class Meta:
-        ordering = ('sub_category',)
+        verbose_name = 'subcategories'
+        verbose_name_plural = 'subcategories'
+        ordering = ['sub_category']
 
     def __str__(self):
         return self.sub_category
 
 
 class Product(models.Model):
-    sub_category = models.ForeignKey(SubCategorie, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategories, on_delete=models.CASCADE)
     name  = models.CharField(max_length=120)
     price = models.CharField(max_length=120)
     image = models.ImageField(upload_to="products/")
--- products/views.py
+++ products/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Product,Categorie,SubCategorie
+from .models import Product,Categories,SubCategories
 # Create your views here.
 
 class CategoryList(ListView):
-    model = Categorie
+    model = Categories
     template_name = "products/category.html"
 
 
 class SubCategoryList(ListView):
-    model = SubCategorie
+    model = SubCategories
     template_name = "products/subcategory.html"
 
 
 class ProductList(ListView):
     model = Product
     template_name = "products/products.html"
-
